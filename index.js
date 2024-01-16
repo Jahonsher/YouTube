a42e417ec0493a3ed255881629d3fa6dfb7a59ba
Jahonsher <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>
2024-01-16 16:36:41 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,152 @@
+let main = document.querySelector("main");
+let left = document.querySelector("main");
+left.classList=("left");
+let reight = document.querySelector("main");
+reight.classList=("reight");
+
+let h1 = document.createElement ("h1")
+
+h1.textContent = "All"
+
+console.log(h1);
+h1.classList.add("box")
+
+h1.insertAdjacentElement("beforebegin", h1)
+
+reight.appendChild(h1)
+
+// let body = document.querySelector("main");
+
+let h2 = document.createElement ("h2")
+
+h2.textContent = "Yatchs"
+
+console.log(h2);
+h2.classList.add("box1")
+
+h2.insertAdjacentElement("beforebegin", h2)
+
+reight.appendChild(h2)
+
+let h3 = document.createElement ("h3")
+
+h3.textContent = "Boats"
+
+console.log(h3);
+
+h3.classList.add("box1")
+
+h3.insertAdjacentElement("beforebegin", h3)
+
+reight.appendChild(h3)
+
+let h4 = document.createElement("h4")
+
+h4.textContent = "RVs"
+
+console.log(h4);
+
+h4.classList.add("box1")
+
+reight.appendChild(h4)
+
+let h5 = document.createElement("h5")
+
+h5.textContent = "Electrical Enginering"
+
+console.log(h5);
+
+h5.classList.add("box2")
+
+reight.appendChild(h5)
+
+let h6 = document.createElement("h6")
+
+h6.textContent = "Phootopier"
+
+console.log(h6);
+
+h6.classList.add("box3")
+
+reight.appendChild(h6)
+
+// main.appendChild(h5)
+
+let h7 = document.createElement("h7")
+
+h7.textContent = "Cookery"
+
+console.log(h7);
+
+h7.classList.add("box3")
+
+reight.appendChild(h7)
+
+let h8 = document.createElement("h8")
+
+h8.textContent = "Apple"
+
+console.log(h8);
+
+h8.classList.add("box1")
+
+reight.appendChild(h8)
+
+let h9 = document.createElement("h9")
+
+h9.textContent = "CSS"
+
+console.log(h9);
+h9.classList.add("box1")
+
+h9.insertAdjacentElement("beforebegin", h9)
+
+reight.appendChild(h9)
+
+let h10 = document.createElement ("h10")
+
+h10.textContent = "Figma"
+
+console.log(h10);
+
+h10.classList.add("box1")
+
+h10.insertAdjacentElement("beforebegin", h10)
+
+reight.appendChild(h10)
+
+let h11 = document.createElement("h11")
+
+h11.textContent = "Astranomy"
+
+console.log(h11);
+
+h11.classList.add("box3")
+
+reight.appendChild(h11)
+
+let h12 = document.createElement("h12")
+
+h12.textContent = "React routers"
+
+console.log(h12);
+
+h12.classList.add("box4")
+
+reight.appendChild(h12)
+
+let h13 = document.createElement("h13")
+
+h13.textContent = "Motorcycles"
+
+console.log(h13);
+
+h13.classList.add("box4")
+
+reight.appendChild(h13)
+
+// // // // // // // // // // // //
+
+
+
+
